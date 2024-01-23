740638fa99f16c07889ce5c87930ef0f39ac4219
pwgawzynski <pgawzynski.backup@gmail.com>
2024-01-23 09:02:23 +0100

    created constants for Company

---
+++ Company/Constants.ts
@@ -0,0 +1,9 @@
+export class CompanyConstants {
+  static readonly MIN_NAME_LENGTH = 1;
+  static readonly MAX_NAME_LENGTH = 500;
+  static readonly MIN_EMAIL_LENGTH = 1;
+  static readonly MAX_EMAIL_LENGTH = 350;
+  static readonly MIN_NIP_LENGTH = 1;
+  static readonly MAX_PHONE_LENGTH = 20;
+  static readonly MIN_PHONE_LENGTH = 1;
+}
