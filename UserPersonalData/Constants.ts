71e942fa4e61320a9e17bc14548a47d23867bcba
pwgawzynski <pgawzynski.backup@gmail.com>
2024-02-03 18:06:10 +0100

    created constants for user personal data

---
+++ UserPersonalData/Constants.ts
@@ -0,0 +1,14 @@
+export class UserPersonalDataConstants {
+  private static readonly BASE_MIN_LEN = 1;
+  static readonly NAME_MIN_LEN = UserPersonalDataConstants.BASE_MIN_LEN;
+
+  static readonly NAME_MAX_LEN = 70;
+
+  static readonly SURNAME_MIN_LEN = UserPersonalDataConstants.BASE_MIN_LEN;
+
+  static readonly SURNAME_MAX_LEN = 250;
+
+  static readonly PHONE_NUMBER_MIN_LEN = 13;
+
+  static readonly PHONE_NUMBER_MAX_LEN = 13;
+}
