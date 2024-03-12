0455efca6c10e4c25f7fcac023e0b426eede9f3a
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-12 10:31:58 +0100

    added typing for update worker status and position

--- Worker/Requests.ts
+++ Worker/Requests.ts
@@ -1,3 +1,11 @@
+import { Position, Status } from './Enums';
+
 export interface CreateWorkerReqI {
   user: string;
+}
+
+export interface UpdateWorkerStatusOrPositionReqI {
+  worker: string;
+  status?: Status;
+  position?: Position;
 }
