3ecc218afaf45ed381729c6662d8353d355e156b
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-19 20:47:08 +0100

    added name constants

--- Field/Constants.ts
+++ Field/Constants.ts
@@ -6,5 +6,7 @@ abstract class FieldConstants {
   static readonly AREA_MAX_VALUE = 99999.99999;
   static readonly POLISH_ID_MIN_LEN = BASE_MIN_LEN;
   static readonly POLISH_ID_MAX_LEN = 40;
+  static readonly NAME_MIN_LEN = BASE_MIN_LEN;
+  static readonly NAME_MAX_LEN = 100;
 }
 export default FieldConstants;
