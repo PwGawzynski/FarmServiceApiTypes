307859dfa87237b96a19dc86dfa6a089a6d65f59
pwgawzynski <pgawzynski.backup@gmail.com>
2024-03-29 19:39:36 +0100

    added order enums

---
+++ Order/Enums.ts
@@ -0,0 +1,16 @@
+/**
+ * Describes order actual status
+ */
+export enum OrderStatus {
+  Added,
+  Processing,
+  Done,
+}
+
+/**
+ * Describes type of service which relates to the order
+ */
+export enum ServiceType {
+  Harvesting,
+  Transport,
+}
