ed40a359e6db9387e8e4d5cccf15c4a228d1f48b
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-05 17:20:57 +0200

    added update interface

--- Order/Requests.ts
+++ Order/Requests.ts
@@ -12,3 +12,11 @@ export interface UpdateOrderSetPricePerUnit extends Partial<CreateOrderReqI> {
   order: string;
   pricePerUnit: number;
 }
+
+export interface UpdateOrder {
+  order: string;
+  name: string;
+  additionalInfo: string;
+  performanceDate: Date;
+  pricePerUnit: number;
+}
