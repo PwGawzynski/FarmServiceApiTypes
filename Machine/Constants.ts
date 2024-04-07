3249aff8889794accd3e9be0bcb00e94b2ac4a00
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-07 20:58:22 +0200

    added constants for machine

---
+++ Machine/Constants.ts
@@ -0,0 +1,9 @@
+export abstract class MachineConstants {
+  static readonly NAME_MIN_LEN = 1;
+
+  static readonly NAME_MAX_LEN = 100;
+
+  static readonly LICENCE_PLATE_MIN_LEN = 1;
+
+  static readonly LICENCE_PLATE_MAX_LEN = 20;
+}
