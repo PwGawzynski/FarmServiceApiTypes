62f3b347ef071015a92dd25c781943b1dcf48d1a
pwgawzynski <pgawzynski.backup@gmail.com>
2024-04-15 16:54:36 +0200

    updated name, changed neasted

---
+++ Task/Responses.ts
@@ -0,0 +1,26 @@
+import { TaskType } from './Enums';
+import { FieldResponseBase } from '../Field/Ressponses';
+import { WorkerResponseBase } from '../Worker/Responses';
+import { MachineResponseBase } from '../Machine/Responses';
+
+export interface TaskResponseBase {
+  id: string;
+
+  isDone?: boolean;
+
+  type: TaskType;
+
+  createdAt: Date;
+
+  openedAt?: Date;
+
+  closedAt?: Date;
+
+  field: FieldResponseBase;
+
+  worker: WorkerResponseBase;
+
+  machine: MachineResponseBase;
+}
+
+export type TaskResponseCollection = Array<TaskResponseBase>;
