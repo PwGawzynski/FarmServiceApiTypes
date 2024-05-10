ca94152e0a8c3e34d7117fef4b6b8dcb3e867d01
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-10 18:58:37 +0200

    updated req

--- OrderPricing/Requests.ts
+++ OrderPricing/Requests.ts
@@ -2,7 +2,6 @@ import { TaskType } from '../Task/Enums';
 
 export interface CreateOrderPriceReqI {
   order: string;
-  taskType: TaskType;
   prices: Map<keyof typeof TaskType, number>;
   tax: number;
 }
