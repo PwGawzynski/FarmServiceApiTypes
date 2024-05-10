4272fab2a82cf8bdd617dd8f967d47ce00dba9c2
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-10 19:37:43 +0200

    added constants

--- OrderPricing/Constants.ts
+++ OrderPricing/Constants.ts
@@ -2,6 +2,6 @@ export abstract class OrderPricingConstants {
   static readonly MAX_PRICE = 99999999.99;
   static readonly MIN_PRICE = 0.0;
   static readonly MAX_TAX = 0.99;
-  static readonly MIN_TAX = 0.0;
+  static readonly MIN_TAX = 0.01;
   static readonly DECIMAL_PLACES = 2;
 }
