165295eac76353dbafd924acbf2e4a4e7cbf9598
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-10 20:44:32 +0200

    added translations

--- Order/Ressponses.ts
+++ Order/Ressponses.ts
@@ -1,5 +1,4 @@
 import { OrderStatus } from './Enums';
-import { TaskType } from '../Task/Enums';
 import { OrderPriceResponseBase } from '../OrderPricing/Responses';
 
 export interface OrderResponseBase {
