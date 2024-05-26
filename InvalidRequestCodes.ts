357c5ea1c6f965206fa48f5e50b7b121abca45e2
pwgawzynski <pgawzynski.backup@gmail.com>
2024-05-26 15:45:39 +0200

    changed logic, error codes in one place

---
+++ InvalidRequestCodes.ts
@@ -0,0 +1,7 @@
+export enum InvalidRequestCodes {
+  account_emailTaken = 1,
+  personalData_phoneTaken = 2,
+  machine_licencePlateTaken = 3,
+  machine_doesNotBelongToCompany = 4,
+  client_userNotCreated = 5,
+}
--- Machine/ErrorCodes.ts
+++
@@ -1,4 +0,0 @@
-export enum MachineErrorCodes {
-  LicencePlateAlreadyTaken = 1,
-  MachineDoesNotBelongToCompany = 2,
-}
